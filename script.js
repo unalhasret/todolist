86c037077fb2a6a4ddee61c0bddf352fc3893c76
Hasret Unal <s.hasretunal@gmail.com>
2022-07-20 15:23:12 +0300

    checked class added to elements

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let inputDOM = document.querySelector("#task");
 let listDOM = document.querySelector("#list");
+let liDOM = document.querySelectorAll("li");
 
 // Ekle butonu
 function newElement(){
     let liDOM = document.createElement("li");
     liDOM.innerHTML = inputDOM.value;
     listDOM.appendChild(liDOM);
-
 }
+
+for(let i=0; i<liDOM.length; i++){
+    liDOM[i].addEventListener("click", function(){
+        liDOM[i].classList.toggle("checked");
+    });
+}
+

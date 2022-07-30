8d2557b8a97fccc22dcc645937764682291f5e73
Hasret Unal <s.hasretunal@gmail.com>
2022-07-30 10:57:27 +0300

    toast updated

--- script.js
+++ script.js
@@ -8,30 +8,29 @@ function newElement(){
         $('.error').toast("show")
     }
     else{
-        $('.success').toast("show")
-    }
+        $('.success').toast("show");
+        let li = document.createElement("li");
+        li.innerHTML = inputDOM.value;
+        ulDOM.appendChild(li);
+        inputDOM.value = "";
 
-    let li = document.createElement("li");
-    li.innerHTML = inputDOM.value;
-    ulDOM.appendChild(li);
-    inputDOM.value = "";
+        // close 
+        let span = document.createElement("span");
+        let txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
 
-    // close 
-    let span = document.createElement("span");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+        //remove
+        span.onclick = function(){
+            this.parentElement.remove();
+        }
 
-    //remove
-    span.onclick = function(){
-        this.parentElement.remove();
-    }
-
-    //checked class
-    li.onclick = function(){
-        this.classList.toggle("checked");
-    }
+        //checked class
+        li.onclick = function(){
+            this.classList.toggle("checked");
+        }
+        }
 }
 
 

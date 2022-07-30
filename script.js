75cf8deb3f4e00e44d2cd31c1dd8879dc2d4a887
Hasret Unal <s.hasretunal@gmail.com>
2022-07-30 10:43:43 +0300

    close, remove add and padding fixed

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 let inputDOM = document.querySelector("#task");
-let ulDOM = document.querySelector("#list");
-let liDOM = document.querySelectorAll("li");
+let ulDOM = document.querySelector("ul");
 
-// Ekle butonu
+// add button
 function newElement(){
     let li = document.createElement("li");
     li.innerHTML = inputDOM.value;
     ulDOM.appendChild(li);
     inputDOM.value = "";
+
+    // close 
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    //remove
+    span.onclick = function(){
+        this.parentElement.remove();
+    }
+    
+    //checked class
+    li.onclick = function(){
+        this.classList.toggle("checked");
+    }
 }
 
-for(let i=0; i<liDOM.length; i++){
-    liDOM[i].addEventListener("click", function(){
-        liDOM[i].classList.toggle("checked");
-    });
-}
+
+
+
 
